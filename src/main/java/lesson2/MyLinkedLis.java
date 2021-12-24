b7256077c61a5444083651f3c25e0e20ef808dbf
vjacheslav <devh@fiatum.com>
2021-12-24 23:14:57 +0300

    Done lesson2

---
+++ src/main/java/lesson2/MyLinkedLis.java
@@ -0,0 +1,194 @@
+package lesson2;
+
+import java.util.NoSuchElementException;
+
+public class MyLinkedLis<T> {
+    private Node first;
+    private Node last;
+    private int size;
+
+    private class Node {
+        Node prev;
+        T value;
+        Node next;
+
+        public Node(Node prev, T value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(T value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
+
+    public void insertFirst(T item) {
+        Node newNode = new Node(item, first);
+        if (isEmpty()) {
+            last = newNode;
+        } else {
+            first.setPrev(newNode);
+        }
+        first = newNode;
+        size++;
+    }
+
+    public T removeFirst() {
+        T temp = getFirst();
+        first = first.getNext();
+        if (isEmpty()) {
+            last = null;
+        } else {
+            first.setPrev(null);
+        }
+        size--;
+        return temp;
+    }
+
+    public void insertLast(T item) {
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            newNode.setPrev(last);
+            last.setNext(newNode);
+        }
+        last = newNode;
+        size++;
+    }
+
+    public T removeLast() {
+        T temp = getLast();
+        if (last.getPrev() == null) {
+            first = null;
+        } else {
+            last.getPrev().setNext(null);
+        }
+        last = last.getPrev();
+        size--;
+        return temp;
+    }
+
+    public T getFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty");
+        }
+        return first.value;
+    }
+
+    public T getLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty");
+        }
+        return last.value;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public final int indexOf(T item) {
+        Node current = first;
+        int index = 0;
+        while (current != null) {
+            if (current.value.equals(item)) {
+                return index;
+            }
+            current = current.getNext();
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(T item) {
+        return indexOf(item) > -1;
+    }
+
+    public void insert(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IllegalStateException("index: " + index);
+        }
+        if (index == 0) {
+            insertFirst(item);
+            return;
+        }
+        if (index == size) {
+            insertLast(item);
+            return;
+        }
+
+        Node current = first;
+        int i = 0;
+        while (i < index - 1) {
+            current = current.getNext();
+            i++;
+        }
+        Node newNode = new Node(current, item, current.getNext());
+        current.getNext().setPrev(newNode);
+        current.setNext(newNode);
+        size++;
+    }
+
+    public boolean remove(T item) {
+        if (isEmpty()) {
+            return false;
+        }
+        if (first.value.equals(item)) {
+            removeFirst();
+            return true;
+        }
+
+        Node current = first;
+        while (current != null && !current.getValue().equals(item)) {
+            current = current.getNext();
+        }
+        if (current == null) {
+            return false;
+        }
+        if (current == last) {
+            removeLast();
+            return true;
+        }
+
+        current.getPrev().setNext(current.getNext());
+        current.getNext().setPrev(current.getPrev());
+        size--;
+        return true;
+    }
+}
+
+
